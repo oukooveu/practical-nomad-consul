379b38d0b625792d56c2ad44e96bee3db4066145
Daniil Kupchenko <kupchenko@gmail.com>
2022-10-12 19:41:17 +0300

    chore: runall script

---
+++ runall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euo pipefail
+
+docker-compose down
+docker-compose up -d
+curl --request PUT --data @external-services/postgres.json 172.16.1.101:8500/v1/catalog/register
+
+nomad job run jobs/cli-jobs/petclinic-web.nomad
+nomad job run jobs/cli-jobs/petclinic-egw.nomad
+nomad job run jobs/cli-jobs/petclinic-api.nomad
+nomad job run jobs/cli-jobs/petclinic-ingw.nomad
