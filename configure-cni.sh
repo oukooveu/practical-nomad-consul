c4717f1350c675655cb587db5df88998bc43601e
Daniil Kupchenko <kupchenko@gmail.com>
2022-10-13 11:04:23 +0300

    chore: silent curl

--- configure-cni.sh
+++ configure-cni.sh
@@ -1,4 +1,4 @@
-curl -L -o cni-plugins.tgz "https://github.com/containernetworking/plugins/releases/download/v0.9.0/cni-plugins-linux-$( [ $(uname -m) = aarch64 ] && echo arm64 || echo amd64)"-v0.9.0.tgz
+curl -sSL -o cni-plugins.tgz "https://github.com/containernetworking/plugins/releases/download/v0.9.0/cni-plugins-linux-$( [ $(uname -m) = aarch64 ] && echo arm64 || echo amd64)"-v0.9.0.tgz
 sudo mkdir -p /opt/cni/bin
 sudo tar -C /opt/cni/bin -xzf cni-plugins.tgz
 
