e56c0c746acbbe9f9e8c3866974ccfb76517d24e
Daniil Kupchenko <kupchenko@gmail.com>
2022-10-17 19:28:12 +0300

    fix: podman host address

--- pipeline-scripts/main.tf
+++ pipeline-scripts/main.tf
@@ -1,13 +1,12 @@
 provider "nomad" {
   #address = "http://127.0.0.1:4646"
-  address = "http://host.docker.internal:4646"
+  address = "http://host.containers.internal:4646"
   #region  = "dc1"
 }
-# Addding the line 
-# Register a job
+
 resource "nomad_job" "petclinic_web" {
   #count = var.angular-artifact-id != "" ? 1 : 0
-    hcl2 {
+  hcl2 {
     enabled  = true
     vars = {
       "angular-artifact-url" = "http://${var.artifact-user}:${var.artifact-pass}@${var.angular-artifact-suffix}/${var.angular-artifact-id}"
@@ -27,5 +26,5 @@ resource "nomad_job" "petclinic_api" {
     }
   }
   jobspec = file("${path.root}/../jobs/cd-jobs/petclinic-api.nomad")
-  
+
 }
